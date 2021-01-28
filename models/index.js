0b700af3cb1033a129d98444c30dec4f344f02d8
Hassan Ali Khan <hassanalikhan87@gmail.com>
2021-01-28 23:06:44 +0500

    17

--- models/index.js
+++ models/index.js
@@ -7,11 +7,12 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-console.log(config);
+// console.log(config);
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log(config.use_env_variable);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
@@ -21,6 +22,7 @@ if (config.use_env_variable) {
     config,
   );
 }
+// console.log(sequelize);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
